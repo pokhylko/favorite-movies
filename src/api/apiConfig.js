ccd57831f9247957ea8300c2491661a9c0b789f8
Anatolii Pokhylko <pokhylko@gmail.com>
2021-10-20 23:08:56 +0300

    feat: add api config file

---
+++ src/api/apiConfig.js
@@ -0,0 +1,6 @@
+export const API_CONFIG = {
+  baseUrl: 'https://api.themoviedb.org/3/',
+  apiKey: '3f8bf2c9426f98928ace8a5e8546dac7',
+  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
+};
