7932bdf0d46604da7c7ed6a0a9d68707b37b8a48
Anatolii Pokhylko <pokhylko@gmail.com>
2023-03-29 18:22:35 +0300

    feat: create Rating component

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,3 +1,27 @@
 import {PlayIcon} from './PlayIcon';
+import {Star0Icon} from './Star0Icon';
+import {Star1Icon} from './Star1Icon';
+import {Star2Icon} from './Star2Icon';
+import {Star3Icon} from './Star3Icon';
+import {Star4Icon} from './Star4Icon';
+import {Star5Icon} from './Star5Icon';
+import {Star6Icon} from './Star6Icon';
+import {Star7Icon} from './Star7Icon';
+import {Star8Icon} from './Star8Icon';
+import {Star9Icon} from './Star9Icon';
+import {Star10Icon} from './Star10Icon';
 
-export {PlayIcon};
+export {
+    PlayIcon,
+    Star0Icon,
+    Star1Icon,
+    Star2Icon,
+    Star3Icon,
+    Star4Icon,
+    Star5Icon,
+    Star6Icon,
+    Star7Icon,
+    Star8Icon,
+    Star9Icon,
+    Star10Icon
+};
---
+++ src/components/Slider/components/Rating/Rating.tsx
@@ -0,0 +1,61 @@
+import {FC, Fragment, useEffect, useState} from 'react';
+
+import {
+    Star0Icon,
+    Star10Icon,
+    Star1Icon,
+    Star2Icon,
+    Star3Icon,
+    Star4Icon,
+    Star5Icon,
+    Star6Icon,
+    Star7Icon,
+    Star8Icon,
+    Star9Icon,
+} from "../../../Icons";
+
+export interface Props {
+    voteAverage: number;
+}
+
+type Grade = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+
+const STAR_ICONS = {
+    0: <Star0Icon/>,
+    1: <Star1Icon/>,
+    2: <Star2Icon/>,
+    3: <Star3Icon/>,
+    4: <Star4Icon/>,
+    5: <Star5Icon/>,
+    6: <Star6Icon/>,
+    7: <Star7Icon/>,
+    8: <Star8Icon/>,
+    9: <Star9Icon/>,
+    10: <Star10Icon/>
+}
+
+export const Rating: FC<Props> = ({voteAverage}) => {
+    const [starRating, setStarRating] = useState<Grade[]>([]);
+
+    useEffect(() => {
+        const [intRating, decRating] = voteAverage.toString().split('.')
+        const ratingArray: Grade[] = [];
+
+        for (let i = 1; i <= 10; i += 1) {
+            if (voteAverage >= i) {
+                ratingArray.push(10);
+            } else if (+intRating + 1 === i && voteAverage <= i) {
+                ratingArray.push(+(decRating[0]) as Grade)
+            } else {
+                ratingArray.push(0)
+            }
+        }
+
+        setStarRating(ratingArray);
+    }, [voteAverage]);
+
+    return (
+        // eslint-disable-next-line react/no-array-index-key
+        <div>{starRating.map((rating, index) => <Fragment key={index}>{STAR_ICONS[rating]}</Fragment>)}</div>
+    );
+};
