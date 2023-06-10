27fbb0772c86ea283544ce4ba46d51f281ffdf45
Anatolii Pokhylko <pokhylko@gmail.com>
2023-06-10 14:00:22 +0300

    feat: change api to tmdb

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-    baseUrl: 'https://api.trakt.tv',
-    apiKey: process.env.REACT_APP_TRAKT_API_KEY ?? "",
+    baseUrl: 'https://api.themoviedb.org/3',
+    apiKey: process.env.REACT_APP_TMDB_API_KEY,
     originalPosterImage: (imgPath: string) =>
         `https://image.tmdb.org/t/p/original${imgPath}`,
     w500PosterImage: (imgPath: string) =>
--- src/services/api.ts
+++ src/services/api.ts
@@ -5,9 +5,8 @@ import {API_CONFIG} from "../api/apiConfig";
 const baseQuery = fetchBaseQuery({
     baseUrl: API_CONFIG.baseUrl,
     prepareHeaders: (headers) => {
-        headers.set("Content-Type", "application/json");
-        headers.set("trakt-api-version", "2");
-        headers.set("trakt-api-key", API_CONFIG.apiKey);
+        headers.set("accept", "application/json");
+        headers.set("Authorization", `Bearer ${API_CONFIG.apiKey}`);
 
         return headers;
     },
@@ -18,6 +17,6 @@ const baseQueryWithRetry = retry(baseQuery, {maxRetries: 6});
 export const api = createApi({
     reducerPath: "splitApi",
     baseQuery: baseQueryWithRetry,
-    tagTypes: ["Popular movies"],
+    tagTypes: ["popularMovies"],
     endpoints: () => ({}),
 });
